db7c899fc47b6c4746b9b9a683a119d06d131ae0
Sowmya Sathu <sonusowmya956@gmail.com>
2020-07-19 11:59:24 +0530

    EpamTask-Collections

---
+++ EpamCollections/src/test/java/code/com/EpamCollections/AppTest.java
@@ -0,0 +1,20 @@
+package code.com.EpamCollections;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
